1ad0f6889b30d327ab11a3666534f88fafd89f51
Diego Coñuenao <diego4124@gmail.com>
2024-06-10 02:38:27 -0400

    Desplegables Especialidad y Hora

--- app/src/main/java/com/example/cita_medica/Reservas.kt
+++ app/src/main/java/com/example/cita_medica/Reservas.kt
@@ -7,10 +7,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,11 +33,52 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Reservas(navController: NavController){
 
+
+    var showDialogReserva by remember {
+        mutableStateOf(false)
+    }
+
+    var showDialogConfirmar by remember {
+        mutableStateOf(false)
+    }
+    /*
+    if (showDialog){
+        AlertDialog(
+            onDismissRequest = {
+                showDialog = false
+            },
+            confirmButton = {
+                TextButton(onClick = { }) {
+                    Text(text = "Actualizar")
+                    }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    showDialog = false
+                }) {
+                    Text(text = "Cancelar")
+                }
+            },
+            title = { Text("Confirmar")},
+            text = {
+                Text(text = "Seguro")
+            }
+        )
+    }
+
+     */
+
     var run by remember {
         mutableStateOf("")
+    }
+
+    var isExpandedEsp by remember {
+        mutableStateOf(false)
     }
 
     var especialidad by remember {
@@ -38,7 +89,15 @@ fun Reservas(navController: NavController){
         mutableStateOf("")
     }
 
+    var isExpandedHora by remember {
+        mutableStateOf(false)
+    }
+
     var horaReserva by remember {
+        mutableStateOf("")
+    }
+
+    var datosReserva by remember {
         mutableStateOf("")
     }
 
@@ -67,11 +126,61 @@ fun Reservas(navController: NavController){
         //Seleccion de Especialidad
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Seleccione Especialidad")
+
+        ExposedDropdownMenuBox(
+            expanded = isExpandedEsp,
+            onExpandedChange = {isExpandedEsp = it})
+        {
+            TextField(
+                value = especialidad,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpandedEsp)
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                modifier = Modifier.menuAnchor()
+            )
+
+            ExposedDropdownMenu(
+                expanded = isExpandedEsp,
+                onDismissRequest = { isExpandedEsp=false }
+            ) {
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Cirujano")
+                    },
+                    onClick = {
+                        especialidad = "Cirujano"
+                        isExpandedEsp = false
+
+                    }
+                )
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Dentista")
+                    },
+                    onClick = {
+                        especialidad = "Dentista"
+                        isExpandedEsp = false
+
+                    }
+                )
+
+            }
+
+
+        }
+
+
+        /*
         OutlinedTextField(value = especialidad, onValueChange = {
             especialidad = it
         }, label = {
             Text(text = "Especialidad")
         })
+
+         */
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -89,20 +198,186 @@ fun Reservas(navController: NavController){
         //Seleccion de Hora
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Seleccione Hora")
+
+        ExposedDropdownMenuBox(
+            expanded = isExpandedHora,
+            onExpandedChange = {isExpandedHora = it})
+        {
+            TextField(
+                value = horaReserva,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpandedHora)
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                modifier = Modifier.menuAnchor()
+            )
+
+            ExposedDropdownMenu(
+                expanded = isExpandedHora,
+                onDismissRequest = { isExpandedHora=false }
+            ) {
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "08:00")
+                    },
+                    onClick = {
+                        horaReserva = "08:00"
+                        isExpandedHora = false
+
+                    }
+                )
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "08:30")
+                    },
+                    onClick = {
+                        horaReserva = "08:30"
+                        isExpandedHora = false
+
+                    }
+                )
+
+            }
+
+
+        }
+        /*
         OutlinedTextField(value = horaReserva, onValueChange = {
             horaReserva = it
         }, label = {
             Text(text = "Hora")
         })
+        */
 
         Spacer(modifier = Modifier.height(16.dp))
 
-
-        Button(onClick = {
-            navController.navigate("Reservas")
-        }) {
-            Text(text = "Reservar")
+        Button(onClick = { showDialogReserva = true
+            //navController.navigate("Reservas")
         }
+
+
+        ) {
+            Text(text = "Reservar")
+            if (showDialogReserva){
+                AlertDialog(
+                    onDismissRequest = {
+                        //showDialogReserva = false
+                    },
+                    confirmButton = {
+                        TextButton(onClick = {showDialogConfirmar = true})
+                        {
+                            Text(text = "Confirmar Reserva")
+
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(onClick = {
+                            showDialogReserva = false
+                        }) {
+                            Text(text = "Cancelar")
+                        }
+                    },
+
+                    title = { Text("Datos de Reserva")},
+
+                    text = {
+                        Column {
+                            Text(text = "RUN: " + run)
+                            Text(text = "Especialidad: " + especialidad)
+                            Text(text = "Fecha: " + fechaReserva)
+                            Text(text = "Hora: " + horaReserva)
+                            datosReserva=run + especialidad + fechaReserva + horaReserva
+                        }
+                    }
+
+                )
+            }
+            if (showDialogConfirmar){
+                AlertDialog(
+                    onDismissRequest = {
+                        //showDialogConfirmar = false
+                    },
+                    confirmButton = {
+                        TextButton(onClick = {showDialogConfirmar=false }) {
+                            Text(text = "Listo")
+
+                        }
+                    },
+                    text = {
+                        Column {
+                            Text(text = "¡Reserva realizada con Éxito!")
+                            showDialogReserva=false
+                            run=""
+                            especialidad=""
+                            fechaReserva=""
+                            horaReserva=""
+                        }
+                    }
+
+                )
+            }
+        }
+        /*
+        //--------------------------
+
+        if (showDialogReserva){
+            AlertDialog(
+                onDismissRequest = {
+                    //showDialogReserva = false
+                },
+                confirmButton = {
+                    TextButton(onClick = {showDialogConfirmar = true})
+                    {
+                        Text(text = "Confirmar Reserva")
+
+                        if (showDialogConfirmar){
+                            AlertDialog(
+                                onDismissRequest = {
+                                    //showDialogConfirmar = false
+                                },
+                                confirmButton = {
+                                    TextButton(onClick = {showDialogConfirmar=false }) {
+                                        Text(text = "Listo")
+
+                                    }
+                                },
+                                text = { Column {
+                                    Text(text = "¡Reserva realizada con Éxito!")
+                                    //showDialogReserva=false
+                                }
+                                }
+
+                            )
+                        }
+
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        showDialogReserva = false
+                    }) {
+                        Text(text = "Cancelar")
+                    }
+                },
+
+                title = { Text("Datos de Reserva")},
+
+                text = {
+                    Column {
+                        Text(text = "RUN: " + run)
+                        Text(text = "Especialidad: " + especialidad)
+                        Text(text = "Fecha: " + fechaReserva)
+                        Text(text = "Hora: " + horaReserva)
+                    }
+                }
+
+            )
+        }
+
+         */
+        //---------------------------
 
         Spacer(modifier = Modifier.height(32.dp))
 
@@ -114,5 +389,6 @@ fun Reservas(navController: NavController){
             Text(text = "Ir a Login")
         }
     }
-    
+
 }
+
